44ca1883089de97025068b57f6fd3ee028b1a607
Joshguyman <61127872+Joshguyman@users.noreply.github.com>
2022-03-15 17:54:41 -0400

    formatted code
    i did thing!

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cstdlib>
 #include <raylib.h>
 #include <ctime>
 #include <iostream>
@@ -77,33 +79,281 @@ class Button {
     }
 
 };
+class Player {
+    Vector2 position;
+    Vector2 hitboxsize;
+    Vector2 vel;
+    float speed;
+    Color color;
+    int healthvalue;
+    int coinammt;
+    float weaponlength;
+    Color weaponcolor;
+                                                        
+
+    public:
+
+    void DrawPlayer(){
+        DrawRectangleV(position, hitboxsize, color);
+    }
+    void DrawWeapon() {
+        Vector2 weaponhitbox = Vector2Divide(hitboxsize, {1, 3});
+        Vector2 dir = Vector2Subtract(GetMousePosition(), position);
+        Vector2 pos = position;
+        if(abs(dir.x) > abs(dir.y)){
+            float temp = weaponhitbox.x;
+            weaponhitbox.x = weaponhitbox.y;
+            weaponhitbox.y = temp;
+            if(dir.x >  0){
+                pos.x += hitboxsize.x + 5;
+            } else {
+                pos.x -= weaponhitbox.x + 5;
+            }
+        } else {
+           if(dir.y >  0){
+                pos.y += hitboxsize.y + 5;
+            } else {
+                pos.y -= weaponhitbox.y + 5;
+            } 
+        }
+        DrawRectangleV(pos, weaponhitbox, weaponcolor);
+    }
+    
+    void UpdatePosition(float dt){
+       position = Vector2Add(position, Vector2Scale(vel, speed * dt));
+    }
+    void AddVel(Vector2 add){
+       vel = Vector2Add(vel, add); 
+    }
+    void SetVel(Vector2 set){
+        vel = set;
+    }
+    Vector2 GetVel(){
+        return vel;
+    }
+    void AddSpeed(float add, int maxspeed){
+        speed += add;
+        if(speed > maxspeed) {
+            speed = maxspeed; 
+        }
+    }
+    void SubSpeed(float sub){
+        speed -= sub;
+        if(speed < 0){
+            speed = 0;
+        }
+    }
+    Vector2 GetPos() {
+        return position;
+    }
+
+    
+    Player(Vector2 sp, Vector2 s, Color c, Color wc, int h, int ca) {
+        position = sp;
+        hitboxsize = s;
+        color = c;
+        healthvalue = h;
+        coinammt = ca;
+        speed = 0;
+        vel = {0, 0};
+        weaponcolor = wc;
+        weaponlength = 45;
+    }
+
+};
+class Enemy {
+    Vector2 size;
+    Vector2 position;
+    Vector2 vel;
+    int health;
+    int type;
+    Color color;
+
+    public:
+
+    void DrawEnemy() {
+        DrawRectangleV(position, size, color);
+    }
+    Vector2 GetPos(){
+        return position;
+    }
+    
+    void MoveEnemy(Vector2 target, float dt) {
+        Vector2 dir = Vector2Subtract(target, position);
+        if(target.x >= position.x){
+            position.x += vel.x * dt;
+        }
+        else if(target.x <= position.x){
+            position.x -= vel.x * dt;
+        }
+        else{
+            position.x += 0;
+        }
+        if(target.y >= position.y){
+            position.y += vel.y * dt;
+        } else if(target.y <= position.y){
+            position.y -= vel.y * dt;
+        }
+        else{
+            position.x += 0;
+        }
+    }
+    
+    Enemy(Vector2 p, int t) {
+        size = {50, 50};
+        position = p;
+        type = t;
+        health = t + 1;
+        color = BLUE;
+        vel = {200, 200};
+    }
+
+};
+
+class SpriteSheet {
+    Texture2D sprite;
+    Vector2 pos;
+    Rectangle framerec;
+    int framescounter;
+    int framesspeed;
+    int numframes;
+    bool visable;
+
+    public:
+
+    int GetFrameSpeed() {
+        return framesspeed;
+    }
+
+    void IncrementFrameCount(){
+        framescounter++;
+    }
+    void ResetFrameCount(){
+        framescounter = 0;
+    }
+    void SetVisable(bool tf){
+        visable = tf;
+    }
+    bool GetVisable(){
+        return visable;
+    }
+    void DrawSprite();
+
+    SpriteSheet(Texture2D s, Vector2 p, int fc, int fs, int nf) {
+        sprite = s;
+        pos = p;
+        framescounter = fc;
+        framesspeed = fs;
+        numframes = nf;
+        framerec = {0, 0, (float)sprite.width/numframes, (float)sprite.height};
+        visable = false;
+    }
+
+};
 int main(void)
 {
     const int screenWidth = 1600;
     const int screenHeight = 900;
+    const int maxspeed = 800;
     InitWindow(screenWidth, screenHeight, "Poopnut");
     Font defaultfont = GetFontDefault();
     Texture2D logo =  LoadTexture("assets/LaserSwords.png");
     Texture2D background = LoadTexture("assets/Stars.png");
+    int scenenumber = 0;
 
     //ui things
     Button Start({340, 80}, {screenWidth/2, screenHeight/2 + 200}, DARKGRAY, BLANK, YELLOW, "START", 70, defaultfont);
     Button Tutorial({340, 80}, {screenWidth/2, screenHeight/2 + 280}, DARKGRAY, BLANK, YELLOW, "TUTORIAL", 70, defaultfont);
+    Button Back({170, 40}, {87, 22}, DARKGRAY, BLANK, BLACK, "BACK", 35, defaultfont);
+
+    Player P({0, 0}, {50, 50}, RED, BLUE, 3, 0);
+    Enemy E({screenWidth/2, screenHeight/2}, 1);
 
     
     
     SetTargetFPS(60);
+    SetExitKey(0);
     while (!WindowShouldClose())
     {
+        float deltatime = GetFrameTime();
         BeginDrawing();
-        ClearBackground(BLACK);
-        DrawTexture(background, 0, 0, WHITE);
-        DrawTextureEx(logo, {screenWidth/2 - 16 * 34, screenHeight/2 - 20 * 31.5}, 0, 35, WHITE);
-        //Start Menu Garbage
-        Start.DrawButton(defaultfont);
-        Tutorial.DrawButton(defaultfont);
-        Start.UpdateButton();
-        Tutorial.UpdateButton();
+
+        //Scene changes
+        //Start Menu
+        if(scenenumber == 0){
+            ClearBackground(BLACK);
+            DrawTexture(background, 0, 0, WHITE);
+            DrawTextureEx(logo, {screenWidth/2 - 16 * 34, screenHeight/2 - 20 * 31.5}, 0, 35, WHITE);
+            if(Start.IsButtonPressed()){
+                scenenumber = 1;
+            }else if(Tutorial.IsButtonPressed()){
+                scenenumber = 2;
+            }
+            Start.DrawButton(defaultfont);
+            Tutorial.DrawButton(defaultfont);
+            Start.UpdateButton();
+            Tutorial.UpdateButton();
+        //Game scene
+        } else if(scenenumber == 1){
+            ClearBackground(WHITE);
+            Back.DrawButton(defaultfont);
+            Back.UpdateButton();
+
+            bool didmove = false;
+            P.SetVel({0, 0});
+
+            if(IsKeyDown(KEY_W)){
+                P.AddVel({0, -1});
+                didmove = true;
+
+            }
+            if(IsKeyDown(KEY_S)){
+                P.AddVel({0, 1});
+                didmove = true;
+
+            }
+            if(IsKeyDown(KEY_A)){
+                P.AddVel({-1, 0});
+                didmove = true;
+
+            }
+            if(IsKeyDown(KEY_D)){
+                P.AddVel({1, 0});
+                didmove = true;
+
+            }
+            P.SetVel(Vector2Normalize(P.GetVel()));
+
+            if(didmove){
+                P.AddSpeed(maxspeed/.125 * deltatime, maxspeed);
+            }
+            else{
+                P.SubSpeed(maxspeed/.125 * deltatime);
+            }
+
+            P.UpdatePosition(deltatime);
+            P.DrawPlayer();
+
+            E.MoveEnemy(P.GetPos(), deltatime);
+            SetMousePosition((int)E.GetPos().x, (int)E.GetPos().y);
+            E.DrawEnemy();
+
+            if(IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+                P.DrawWeapon();
+            }
+
+            //Tutorial scene
+        } else if(scenenumber == 2){
+            ClearBackground(PURPLE);
+            Back.DrawButton(defaultfont);
+            Back.UpdateButton();
+        }
+
+        //buttton logic
+
+        if((Back.IsButtonPressed() || IsKeyPressed(KEY_ESCAPE)) && scenenumber != 0){
+            scenenumber = 0;
+        }
         
 
        
